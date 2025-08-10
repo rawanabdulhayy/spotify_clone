7449ac2f4c5ff5f9b8ba85e27c90c9188896e21c
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-08-10 18:16:12 +0300

    podcasts layout set

--- lib/podcasts_preview.dart
+++ lib/podcasts_preview.dart
@@ -12,10 +12,95 @@ class PodcastsPreview extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Now choose some \npodcasts.", style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.w700),),
+            Text(
+              "Now choose some \npodcasts.",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 10),
             SearchBar(
               leading: Icon(Icons.search),
-
+              //the trailing here is a list just like actions have been in appbar
+              // trailing: [],
+              backgroundColor: WidgetStateProperty.all(Colors.white),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              ),
+              hintText: "Search",
+              hintStyle: WidgetStateProperty.all(
+                TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 1,
+                scrollDirection: Axis.vertical,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 40,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/song1.png",
+                          fit: BoxFit.fill,
+                          alignment: Alignment.center,
+                        ),
+                        // Text(),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        // Image.asset(
+                        //   "assets/song1.png",
+                        //   width: 150,
+                        //   height: 150,
+                        // ),
+                        // Text(),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        // Image.asset(
+                        //   "assets/song1.png",
+                        //   width: 150,
+                        //   height: 150,
+                        // ),
+                        // Text(),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        // Image.asset(
+                        //   "assets/song1.png",
+                        //   width: 150,
+                        //   height: 150,
+                        // ),
+                        // Text(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
